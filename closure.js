514ac23a7f14b13d7735754a80805a2154fcf185
Anubhav <anubhv0001@gmail.com>
2025-03-07 20:01:12 +0530

    closureExample

---
+++ closure.js
@@ -0,0 +1,14 @@
+/*Create a simple function outerFunction that defines a variable message within its scope. Return an inner function that logs the message
+when invoked. Demonstrate the concept of closure by invoking the outer function and storing the result in a variable. Call the stored
+function and ensure that it logs the correct message even after the outer function has completed its execution.*/
+
+
+function outerFunction (){
+    var message="Hello";
+    return function innerFunction(){
+      console.log(message)
+    };
+  };
+  let ans=outerFunction();
+  ans();
+   
