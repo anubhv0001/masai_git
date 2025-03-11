0466a2b6a63bcad0090db1b4aa7c61dbe3dd3d69
Anubhav <anubhv0001@gmail.com>
2025-03-12 00:50:09 +0530

    pattern

---
+++ pattern.js
@@ -0,0 +1,11 @@
+function rightAngleTriangle(N) {
+    // Write code here
+    
+    for (let i=1;i<=N;i++){
+      let bag="";
+      for (let j=1;j<=i;j++){
+       bag+=i+" ";
+      }
+      console.log(bag);
+    }
+}
