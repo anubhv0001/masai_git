224c551da6525bbd079a5d6432751688b127ad83
Anubhav <anubhv0001@gmail.com>
2025-03-23 12:01:51 +0530

    testb

---
+++ test2.js
@@ -0,0 +1,17 @@
+constructor studentsManager(name,score,subject){
+    this.name=name,
+    this.score=score,
+    this.subject=subject
+  }
+  const studentDetails={};
+  const addStudents(){
+    studentDetails.push(name);
+  }
+  const score(){
+    studentDetails.push(socre);
+  }
+  const addSubject(){
+    studentDetails.push(subject);
+  }
+  
+  studentDetails.addStudents(Anubhav);
