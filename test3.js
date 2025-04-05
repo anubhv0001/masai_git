369af4a7c9fd50df20dabead12fc9e6634b7eec3
Anubhav <anubhv0001@gmail.com>
2025-04-05 19:03:52 +0530

    cartCode

---
+++ test3.js
@@ -0,0 +1,12 @@
+let cart=[];
+cart.push("Apple");
+cart.push("Banana");
+cart.push("Bread");
+cart.push("Milk");
+let popped=cart.pop();
+
+console.log("Total number of items in cart is:"+ cart.length);
+
+console.log("Items in cart:"+cart);
+
+console.log("Item which is removed:"+popped);
