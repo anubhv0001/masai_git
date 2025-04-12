052ce79dec8cdff8830704edee01898a2aa02c00
Anubhav <anubhv0001@gmail.com>
2025-04-12 17:47:30 +0530

    patternCheckInsubstring

---
+++ checkpattern.js
@@ -0,0 +1,18 @@
+function findMatching(n,m,str,pat){
+    // write code here
+    for(let i=0;i<n-m;i++){
+      let match=true;
+      for (let j=0;j<m;j++){
+        if (str[i+j]!=pat){
+          match=false;
+          break;
+        }
+      }
+      if (match){
+        console.log("Yes");
+        return;
+      }
+    }
+    console.log("No");
+    
+}
