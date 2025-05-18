375944bd1aab68747c34063a8ea4c91925041a29
Anubhav <anubhv0001@gmail.com>
2025-05-18 16:15:27 +0530

    mapDouble

---
+++ mapDouble.js
@@ -0,0 +1,9 @@
+/*Write a function doubleNumbers() that takes an array of numbers and returns a new array where each number is doubled.
+
+Requirements:
+Use .map() to iterate over the array and double each number.
+Example:
+]*/
+  const numbers = [1, 2, 3, 4].map((ele)=>ele*2);
+  console.log(numbers);
+  
