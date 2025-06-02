b0a79d603b7d2072aae686a7c2511c3db44b8359
Anubhav <anubhv0001@gmail.com>
2025-06-02 19:11:35 +0530

    zigzag

---
+++ zigzag DSA.js
@@ -0,0 +1,18 @@
+function goInZigZag(N, M, matrix){
+    //write code here
+    let bag="";
+  for (let i=0;i<N;i++){
+    if (i%2==0){
+      for (let j=M-1;j>=0;j--){
+        bag+=matrix[i][j]+" ";
+      }
+    }
+    else{
+      for (let j=0;j<M;j++){
+        bag+=matrix[i][j]+" ";
+      }
+    }
+    
+  }
+  console.log(bag);
+}
